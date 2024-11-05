a5b27b829f304d4aad8bf63d56e2102305cada33
Michal Popek <michal.dobsice@seznam.cz>
2024-11-05 14:13:38 +0100

    results of dfpap and dfpmp

--- pythonScripts/pythonScripts.py
+++ pythonScripts/pythonScripts.py
@@ -3,45 +3,49 @@ from os import listdir
 import matplotlib.pyplot as plt
 import numpy as np
 
-# files = [f for f in listdir("results/astar")]
+# files = [f for f in listdir("results/astarGrid")]
 
 # print(len(files))
 
 # j = 0
-# sss = [1, 10, 2, 20, 3, 4, 5, 6, 7, 8, 9]
-# ss = sss[j]
-# ttfw = 0.2
+# dfpaps = [0, 1, 10, 2, 3, 4, 5, 6, 7, 8, 9]
+# dfpap = dfpaps[j]
+# dfpmp = 0
 
-# print("ss,ttfw,%win,% travelled,run time,nodes evaluated")
+# print("dfpap,dfpmp,%win,% travelled,run time,game ticks,planning time,total plannings,nodes evaluated,most backtracked nodes")
 
-# for i in range(0, 110):
-#     ss = sss[j]
-#     df = pd.read_csv("results/astar/" + files[i], header=2)
-#     df2 = pd.read_csv("results/astar/" + files[i + 110], header=2)
-#     df3 = pd.read_csv("results/astar/" + files[i + 220], header=2)
-#     print(str(ss) + "," 
-#           + '%.2f' % ttfw + "," + 
-#           '%.2f' % ((df["win/fail"].sum() + df2["win/fail"].sum() + df3["win/fail"].sum()) / 45.0) + "," +
-#           '%.2f' % ((df["% travelled"].sum() + df2["% travelled"].sum() + df3["% travelled"].sum()) / 45.0) + "," +
+# for i in range(0, 143):
+#     dfpap = dfpaps[j]
+#     df = pd.read_csv("results/astarGrid/" + files[i], header=5)
+#     df2 = pd.read_csv("results/astarGrid/" + files[i + 143], header=5)
+#     df3 = pd.read_csv("results/astarGrid/" + files[i + 143 * 2], header=5)
+#     print(str(dfpap) + "," 
+#           + '%.2f' % dfpmp + "," + 
+#           '%.2f' % ((df["win/fail"].sum() + df2["win/fail"].sum() + df3["win/fail"].sum()) / 429) + "," +
+#           '%.2f' % ((df["% travelled"].sum() + df2["% travelled"].sum() + df3["% travelled"].sum()) / 429) + "," +
 #           str((df["run time"].sum() + df2["run time"].sum() + df3["run time"].sum())) + "," +
-#           str((df["nodes evaluated"].sum() + df2["nodes evaluated"].sum() + df3["nodes evaluated"].sum()))
+#           str((df["game ticks"].sum() + df2["game ticks"].sum() + df3["game ticks"].sum())) + "," +
+#           str((df["planning time"].sum() + df2["planning time"].sum() + df3["planning time"].sum())) + "," +
+#           str((df["total plannings"].sum() + df2["total plannings"].sum() + df3["total plannings"].sum())) + "," +
+#           str((df["nodes evaluated"].sum() + df2["nodes evaluated"].sum() + df3["nodes evaluated"].sum())) + "," +
+#           str(max((df["most backtracked nodes"].max(), df2["most backtracked nodes"].max(), df3["most backtracked nodes"].max())))
 #           )
     
-#     ttfw += 0.2;
-#     if ttfw > 2.05:
-#         ttfw = 0.2
+#     dfpmp += 0.25;
+#     if dfpmp > 3.05:
+#         dfpmp = 0
 #         j += 1;       
 #         pass
 
-df = pd.read_csv("resultsConc/astar.csv")
+df = pd.read_csv("resultsConc/astarGrid.csv")
 
-df_fin = df.pivot(index="ss", columns="ttfw", values="% travelled")
+df_fin = df.pivot(index="dfpap", columns="dfpmp", values="most backtracked nodes")
 print(df_fin)
 
 plt.imshow(df_fin, cmap="hot")
-plt.title("% Travelled")
-plt.ylabel("Search steps")
-plt.xlabel("Time to finish weight")
+plt.title("most backtracked nodes")
+plt.ylabel("DFPAP")
+plt.xlabel("DFPMP")
 plt.yticks(range(len(df_fin.index)), df_fin.index)
 plt.xticks(range(len(df_fin.columns)), df_fin.columns)
 plt.colorbar()
