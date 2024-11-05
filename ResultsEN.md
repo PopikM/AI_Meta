4825758c1ae860bcf5cd6bde4702fe3982fa1e15
Michal Popek <michal.dobsice@seznam.cz>
2024-11-05 18:51:46 +0100

    more tests

---
+++ ResultsEN.md
@@ -0,0 +1,52 @@
+# RESULTS
+
+## COMMON TASK
+
+V tomto úkolu jsme měnili SEARCH_STEPS (1, 2, ..., 10, 20) a TIME_TO_FINNISH_WEIGHT(0.2, 0.4, ..., 2.0).
+
+Win Percentages             |  Run Time
+:-------------------------:|:-------------------------:
+![Graph Win Percentages](pythonScripts//resultsConc/astarWinPercentages.png)|![Graph Run Time](pythonScripts/resultsConc/astarRunTime.png)
+
+Z grafů výše si můžeme všimnout, že šance na výhru je nepřímo úměrná času běhu.
+
+`SEARCH_STEPS = 4`  se zdá jako optimální hodnotou, jelikož při nižších hodnotách trávil agent hodně času na přemýšlení a při vyšších vznikala chybovost kvůli přeskakování herních tiků.
+
+Dále vidíme, že při `SEARCH_STEPS=20` agent téměř nevyhrává. Moje hypotéza je, že agent měl problém s navigací a tedy umřel na většině úrovní.
+
+## ELECTIVE TASK - Polynomiální penalizace při odchodu od cesty
+
+Parametry:
+ - NODE_DEPTH_WEIGHT = 1
+ - SEARCH_STEPS = 4 *(zvoleno podle Common Task)*
+ - DISTANCE_FROM_PATH_TOLERANCE = 0
+ - DISTANCE_FROM_PATH_ADDITIVE_PENALTY = (0, 1, ..., 10)
+ - DISTANCE_FROM_PATH_MULTIPLICATIVE_PENALTY = (0, 0.25, ..., 3)
+
+Celkem 143 kombinací parametrů.
+
+Hodnota penalizace: `vzdálenost od cesty * DFPMP ^ 2 + DFPAP ^ 2 + DFPAP`
+
+### Výsledky
+
+Volba parametrů neměla skoro žádný vliv na šanci na výhru. Je možné, že při větším rozdílu hodnot by byl rozdíl výher větší.
+Konkrétně by mohlo mít smysl zvýšit hodnoty DFPMP, protože v rostoucím směru je rostoucí šance na výhru.
+
+![Graph Win Percentages](pythonScripts//resultsConc/astarGridWinPercentage.png)
+
+Zajímavější je ale průběh běhu. Podívejme se na celkový čas. Nejkratší čas vidíme u DFPMP = 1.25 a DFPAP = 1. S rostoucí vzdáleností od tohoto bodu pak narůstá čas.
+Při nízkých hodnotách DFPMP a DFPAP má agent větší volnost při prohledávání, může jít dál od cesty.
+
+![Graph Run Time](pythonScripts/resultsConc/astarGridRunTime.png)
+
+Dále vidíme spojitost mezi počtem projitých uzlů a časem stráveným na výpočtu.
+
+![Graph Nodes](pythonScripts/resultsConc/astarGridNodes.png)
+
+Pokud se podíváme, při jakých parametrech byl největší backtracking, pak to vypadá, že největší backtracking byl při DFPMP=0.5.
+
+![Graph Backtracking](pythonScripts/resultsConc/astarGridMostBacktracked.png)
+
+Další pohled na šanci na výhru a backtracking může ukazovat jistou spojitost mezi těmito hodnotami, konkrétně nepřímou úměrnost.
+Jelikož je rozdíl u procenta výher tak nízký, může toto pozorování být chybné.
+
--- pythonScripts/pythonScripts.py
+++ pythonScripts/pythonScripts.py
@@ -3,26 +3,28 @@ from os import listdir
 import matplotlib.pyplot as plt
 import numpy as np
 
-# files = [f for f in listdir("results/astarGrid")]
+# files = [f for f in listdir("results/astarGrid2")]
 
 # print(len(files))
 
 # j = 0
+# k = 0
 # dfpaps = [0, 1, 10, 2, 3, 4, 5, 6, 7, 8, 9]
 # dfpap = dfpaps[j]
-# dfpmp = 0
+# dfpmp = dfpaps[k]
 
 # print("dfpap,dfpmp,%win,% travelled,run time,game ticks,planning time,total plannings,nodes evaluated,most backtracked nodes")
 
-# for i in range(0, 143):
+# for i in range(0, 121):
 #     dfpap = dfpaps[j]
-#     df = pd.read_csv("results/astarGrid/" + files[i], header=5)
-#     df2 = pd.read_csv("results/astarGrid/" + files[i + 143], header=5)
-#     df3 = pd.read_csv("results/astarGrid/" + files[i + 143 * 2], header=5)
+#     dfpmp = dfpaps[k]
+#     df = pd.read_csv("results/astarGrid2/" + files[i], header=5)
+#     df2 = pd.read_csv("results/astarGrid2/" + files[i + 121], header=5)
+#     df3 = pd.read_csv("results/astarGrid2/" + files[i + 121 * 2], header=5)
 #     print(str(dfpap) + "," 
 #           + '%.2f' % dfpmp + "," + 
-#           '%.2f' % ((df["win/fail"].sum() + df2["win/fail"].sum() + df3["win/fail"].sum()) / 429) + "," +
-#           '%.2f' % ((df["% travelled"].sum() + df2["% travelled"].sum() + df3["% travelled"].sum()) / 429) + "," +
+#           '%.2f' % ((df["win/fail"].sum() + df2["win/fail"].sum() + df3["win/fail"].sum()) / 363) + "," +
+#           '%.2f' % ((df["% travelled"].sum() + df2["% travelled"].sum() + df3["% travelled"].sum()) / 363) + "," +
 #           str((df["run time"].sum() + df2["run time"].sum() + df3["run time"].sum())) + "," +
 #           str((df["game ticks"].sum() + df2["game ticks"].sum() + df3["game ticks"].sum())) + "," +
 #           str((df["planning time"].sum() + df2["planning time"].sum() + df3["planning time"].sum())) + "," +
@@ -31,19 +33,19 @@ import numpy as np
 #           str(max((df["most backtracked nodes"].max(), df2["most backtracked nodes"].max(), df3["most backtracked nodes"].max())))
 #           )
     
-#     dfpmp += 0.25;
-#     if dfpmp > 3.05:
-#         dfpmp = 0
+#     k += 1
+#     if k > 10:
+#         k = 0
 #         j += 1;       
 #         pass
 
-df = pd.read_csv("resultsConc/astarGrid.csv")
+df = pd.read_csv("resultsConc/astarGrid2.csv")
 
 df_fin = df.pivot(index="dfpap", columns="dfpmp", values="most backtracked nodes")
 print(df_fin)
 
 plt.imshow(df_fin, cmap="hot")
-plt.title("most backtracked nodes")
+plt.title("Run time")
 plt.ylabel("DFPAP")
 plt.xlabel("DFPMP")
 plt.yticks(range(len(df_fin.index)), df_fin.index)
